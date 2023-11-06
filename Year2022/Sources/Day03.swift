77bf8148bc43fc15e9c803fa17f4167fb59f76fb
Allister Isaiah Harvey <41553768+a-isaiahharvey@users.noreply.github.com>
2023-11-06 16:37:21 -0400

    complete 2022 day 3

---
+++ Year2022/Sources/Day03.swift
@@ -0,0 +1,47 @@
+//
+// Advent of Code 2022
+//
+// https://adventofcode.com/2022/day/3
+//
+import Advent
+import Algorithms
+
+public enum Day03: Day {
+
+  public static let title = "Rucksack Reorganization"
+
+  public static func part1(_ input: Input) throws -> Int {
+    try input.lines
+      .sum { line in
+        let array = Array(line)
+        let middle = array.count / 2
+        let first = Set(array[..<middle])
+        let last = Set(array[middle...])
+        return try first.intersection(last)
+          .sum(of: Int.priority)
+      }
+  }
+
+  public static func part2(_ input: Input) throws -> Int {
+    try input.lines
+      .chunks(ofCount: 3)
+      .sum { group in
+        try group
+          .map(Set.init)
+          .reduce { $0.intersection($1) }!
+          .sum(of: Int.priority)
+      }
+  }
+}
+
+extension Int {
+
+  fileprivate static func priority(_ character: Character) throws -> Int {
+    let ascii = try Int.ascii(character)
+    switch ascii {
+    case (65...90): return ascii - 38
+    case (97...122): return ascii - 96
+    default: fatalError()
+    }
+  }
+}
---
+++ Year2022/Tests/Day03Tests.swift
@@ -0,0 +1,40 @@
+import Advent
+import XCTest
+import Year2022
+
+final class Day03Tests: XCTestCase {
+
+  func testPart1Examples() throws {
+    XCTAssertEqual(
+      try Day03.part1([
+        "vJrwpWtwJgWrhcsFMMfFFhFp",
+        "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+        "PmmdzqPrVvPwwTWBwg",
+        "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+        "ttgJtRGJQctTZtZT",
+        "CrZsJsPPZsGzwwsLwLmpwMDw",
+      ]), 157)
+  }
+
+  func testPart1Puzzle() throws {
+    let input = try Bundle.module.input(named: "Day03")
+    XCTAssertEqual(try Day03.part1(input), 7428)
+  }
+
+  func testPart2Examples() throws {
+    XCTAssertEqual(
+      try Day03.part2([
+        "vJrwpWtwJgWrhcsFMMfFFhFp",
+        "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+        "PmmdzqPrVvPwwTWBwg",
+        "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+        "ttgJtRGJQctTZtZT",
+        "CrZsJsPPZsGzwwsLwLmpwMDw",
+      ]), 70)
+  }
+
+  func testPart2Puzzle() throws {
+    let input = try Bundle.module.input(named: "Day03")
+    XCTAssertEqual(try Day03.part2(input), 2650)
+  }
+}
