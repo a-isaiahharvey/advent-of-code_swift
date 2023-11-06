8d07201e77cea56bd04b5ef3ebc8fcb2d53aef31
Allister Isaiah Harvey <41553768+a-isaiahharvey@users.noreply.github.com>
2023-11-06 16:37:34 -0400

    complete 2022 day 4

---
+++ Year2022/Sources/Day04.swift
@@ -0,0 +1,42 @@
+//
+// Advent of Code 2022
+//
+// https://adventofcode.com/2022/day/4
+//
+
+import Advent
+import RegexBuilder
+
+public enum Day04: Day {
+
+  public static let title = "Camp Cleanup"
+
+  private static let regex = Regex {
+    TryCapture.integer
+    "-"
+    TryCapture.integer
+    ","
+    TryCapture.integer
+    "-"
+    TryCapture.integer
+  }
+
+  public static func part1(_ input: Input) throws -> Int {
+    try input.lines.count(where: { line in
+      let output = try regex.match(in: line).output
+      let first = output.1...output.2
+      let second = output.3...output.4
+      return first.isSuperset(of: second)
+        || first.isSubset(of: second)
+    })
+  }
+
+  public static func part2(_ input: Input) throws -> Int {
+    try input.lines.count(where: { line in
+      let output = try regex.match(in: line).output
+      let first = output.1...output.2
+      let second = output.3...output.4
+      return first.overlaps(second)
+    })
+  }
+}
---
+++ Year2022/Tests/Day04Tests.swift
@@ -0,0 +1,40 @@
+import Advent
+import XCTest
+import Year2022
+
+final class Day04Tests: XCTestCase {
+
+  func testPart1Examples() throws {
+    XCTAssertEqual(
+      try Day04.part1([
+        "2-4,6-8",
+        "2-3,4-5",
+        "5-7,7-9",
+        "2-8,3-7",
+        "6-6,4-6",
+        "2-6,4-8",
+      ]), 2)
+  }
+
+  func testPart1Puzzle() throws {
+    let input = try Bundle.module.input(named: "Day04")
+    XCTAssertEqual(try Day04.part1(input), 567)
+  }
+
+  func testPart2Examples() throws {
+    XCTAssertEqual(
+      try Day04.part2([
+        "2-4,6-8",
+        "2-3,4-5",
+        "5-7,7-9",
+        "2-8,3-7",
+        "6-6,4-6",
+        "2-6,4-8",
+      ]), 4)
+  }
+
+  func testPart2Puzzle() throws {
+    let input = try Bundle.module.input(named: "Day04")
+    XCTAssertEqual(try Day04.part2(input), 907)
+  }
+}
